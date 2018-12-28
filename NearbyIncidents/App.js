6c4154a3f1c3664021ce3e73de813977a5fd129d
Nikhil Lahoti <lahotinikhil92@gmail.com>
2018-12-28 01:04:31 -0800

    Adding setup changes for react-native-push-notifications

--- NearbyIncidents/App.js
+++ NearbyIncidents/App.js
@@ -5,6 +5,45 @@ import { StackNavigator } from 'react-navigation'
 
 import Routes from './app/routes';
 
+import firebase from './app/helper/FirebaseConnection';
+
+var PushNotification = require('react-native-push-notification');
+
+PushNotification.configure({
+
+  onRegister: function(token){
+    console.warn(token);
+    const root_ref_db = firebase.database().ref();
+    let users = root_ref_db.child('user').child('Nikhil');
+
+    users.set({
+      deviceId: token,
+      yes: "itworked"
+    }).then((data) => {
+      console.warn(token);
+    }).catch((error) => {
+      console.warn(error);
+    });
+  },
+
+  onNotification: function(notification){
+    setTimeout(() => {
+        if(!notification['foreground']){
+          ToastAndroid.show("You've clicked!", ToastAndroid.SHORT)
+        }
+    }, 1);
+    PushNotification.localNotificationSchedule({
+      title: 'Notification with Nikhil Lahoti',
+      message: notification['name'],
+      date: new Date(Date.now())
+    });
+  },
+
+  senderID: "957321769280"
+});
+
+
+
 const App = () => {
   return (
     <AppStackNavigator />
