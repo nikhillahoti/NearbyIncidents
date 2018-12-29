7129f24d1772c4985e7bb9a7a0e5261bb2736ea4
Nikhil Lahoti <lahotinikhil92@gmail.com>
2018-12-29 15:53:43 -0800

    Feature to push notifications to the users device when an incident is added

--- Cloud Functions/functions/src/index.ts
+++ Cloud Functions/functions/src/index.ts
@@ -3,12 +3,31 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 exports.onIncidentAdded = 
-    functions.database.ref("/events/{id}")
+    functions.database.ref("/events/{incidentID}")
         .onCreate((snapshot, context)  => {
-            let newIncidentId = context.params.id;
+            let newIncidentID = context.params.incidentID;
             
-            console.log("New Incident Id is " + newIncidentId);
+            console.log("New Incident Id is " + newIncidentID);
             
             let incidentLocation = admin.database().ref('/incident-location/Lenzen Avenue/');
-            return incidentLocation.child('incidents').push().set(newIncidentId);
+            return incidentLocation.child('incidents').push().set(newIncidentID);
+        });
+
+exports.notifyUser = 
+    functions.database.ref('/incident-location/{location}/incidents/{incidentId}')
+        .onCreate((snapshot, context) => {
+            let usersRef = admin.database().ref('/incident-location/' + context.params.location + "/users")
+            return usersRef.once('value').then((userSnapshot) => {
+                let userR = userSnapshot.val();
+                let userDetailsRef = admin.database().ref(`/users/${userR}/deviceId/token`);
+                return userDetailsRef.once('value').then((token) => {
+                    const payload = {
+                        notification: {
+                            title: 'Nearby Incidents',
+                            body: 'Incident took place on Lenzen Avenue'
+                        }
+                    }
+                    return admin.messaging().sendToDevice(token.val(), payload);
+                });
+            })
         });
